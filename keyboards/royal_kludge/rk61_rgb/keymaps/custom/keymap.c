a74585248d8abccd74f017f9001f3e3b6dae11d0
KaanCanINC <kaan.yazilim@outlook.com>
2024-01-26 20:11:01 +0300

    rk61 custom keymap

--- keyboards/royal_kludge/rk61_rgb/keymaps/custom/keymap.c
+++ keyboards/royal_kludge/rk61_rgb/keymaps/custom/keymap.c
@@ -28,16 +28,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [WINFN] = LAYOUT_60_ansi(
-        KC_TILD,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     QK_BOOT,
-        KC_TRNS,    KC_BTN3,    KC_MS_U,    KC_BTN1,    KC_BTN2,    KC_TRNS,    KC_PSCR,    KC_SCRL,    KC_PAUS,    KC_TRNS,    KC_TRNS,    RGB_VAD,    RGB_VAI,    RGB_MOD,   
+        KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     QK_BOOT,
+        KC_TRNS,    KC_BTN3,    KC_MS_U,    KC_BTN1,    KC_BTN2,    KC_TRNS,    KC_PSCR,    KC_SCRL,    KC_PAUS,    G(KC_BTN1), G(KC_BTN2), RGB_VAD,    RGB_VAI,    RGB_MOD,   
         KC_TRNS,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_U,    KC_TRNS,    KC_INS,     KC_HOME,    KC_PGUP,    KC_TRNS,    RGB_SPD,    RGB_SPI,                KC_TRNS,   
-        KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_WH_D,    KC_TRNS,    KC_TRNS,    KC_DEL,     KC_END,     KC_PGDN,    RGB_HUI,    KC_SLSH,    KC_MOUSE,   
-        KC_TRNS,    KC_TRNS,    KC_TRNS,                                        KC_TRNS,                                        KC_RALT,    KC_APP,     KC_RCTL,    KC_TRNS
+        KC_TRNS,                KC_PSTE,    KC_COPY,    KC_WH_D,    KC_TRNS,    KC_TRNS,    KC_DEL,     KC_END,     KC_PGDN,    RGB_HUI,    KC_SLSH,    KC_RSFT,   
+        KC_TRNS,    KC_TRNS,    KC_TRNS,                                        KC_MOUSE,                                       KC_ALGR,    KC_APP,     KC_RCTL,    KC_TRNS
     ),
 
 
     [MOUSEBASE] = LAYOUT_60_ansi(
-        KC_TILD,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_BASE,    KC_TRNS,
+        KC_GRV,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_BASE,    KC_TRNS,
         KC_TRNS,    KC_BTN3,    KC_MS_U,    KC_BTN1,    KC_BTN2,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   
         KC_TRNS,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_U,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                 KC_TRNS,   
         KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_WH_D,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   
@@ -58,10 +58,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(MOUSEBASE);
             }
             return false;
+       case KC_ESC:
+        if (get_mods() == MOD_BIT(KC_LALT)) {
+            if (record->event.pressed) {
+
+                register_code(KC_COMM);
+            } else {
+                unregister_code(KC_COMM);
+            }
+
+            return false;
+        }
+        return true;
+        case KC_UP:
+        if (get_mods() == MOD_BIT(KC_LALT)) {
+            if (record->event.pressed) {
+
+                register_code(KC_SLSH);
+            } else {
+                unregister_code(KC_SLSH);
+            }
+
+            return false;
+        }
+        return true;
         default:
             return true;  // Process all other keycodes normally
     }
-}
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
